e0c291d051cc03f449b1241a240ffe9cf765ffad
livmari <liv.alterskjaer@icloud.com>
2021-04-01 19:49:20 +0200

    cors and morgan added to app

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 
 const mailRoute = require('./routes/mail')
 
-const app = express()
+// Declaring the port to listen to
 const port = process.env.PORT || 6174
+
+// Create Express app
+const app = express()
 
 // parse application/json
 app.use(express.json())
 
-// To test api
+// HTTP request logger
+app.use(morgan('dev'))
+
+// Enabling cross-origin resource sharing
+app.options('*', cors())
+app.use(cors())
+
+// Importing routes
 app.get('/', async (req, res, next) => {
   try {
-    res.status(200).send('Hello world!')
+    res
+      .status(200)
+      .send(
+        '<h1 style="font-family:sans-serif">💌 Hello from the mailer api!</h1>'
+      )
   } catch (error) {
     next(error)
   }
@@ -20,6 +35,13 @@ app.get('/', async (req, res, next) => {
 
 app.use('/mail', mailRoute)
 
-app.listen(port, () => {
+// Not found message when fails happen
+app.use('/*', (req, res) => {
+  res.status(400).json({ message: 'Not found' })
+})
+
+app.listen(port, error => {
+  if (error) throw error
+
   console.log(`✨ Server listening at port ${port}`)
 })
